f81aee2cc324d945186e8114f733aed32279eabf
bezgin.i <kurt1v@yandex.ru>
2020-10-08 16:45:02 +0300

    change time

--- src/scene/hours-minutes/index.ts
+++ src/scene/hours-minutes/index.ts
@@ -4,23 +4,26 @@ import { mainMenuHelper } from "../../service/utils/main-menu";
 
 const WizardScene = require("telegraf/scenes/wizard");
 
-
 export const settingHoursMinutes = new WizardScene(
     "hours-minutes",
     async ctx => {
-
         const currentTime = moment().format("HH:mm");
 
-        const feautureTime = moment(process.env.ALARM_TIME, "HHmm").format("HH:mm");
+        const feautureTime = moment(1020, "HHmm").format("HH:mm");
 
-        let getDate = (string) => new Date(0, 0, 0, string.split(':')[0], string.split(':')[1]); //получение даты из строки (подставляются часы и минуты
-        let different = Number(getDate(feautureTime)) - Number(getDate(currentTime));
+        let getDate = string =>
+            new Date(0, 0, 0, string.split(":")[0], string.split(":")[1]); //получение даты из строки (подставляются часы и минуты
+        let different =
+            Number(getDate(feautureTime)) - Number(getDate(currentTime));
 
         let hours = Math.floor((different % 86400000) / 3600000);
         let minutes = Math.round(((different % 86400000) % 3600000) / 60000);
-
+        console.log(hours);
+        console.log(minutes);
         await ctx.replyWithHTML(
-            `<strong>До оповещения осталось: ${hours < 0 ? hours * -1 : hours} часов ${minutes < 0 ? minutes * -1 : minutes} минут </strong>`,
+            `До оповещения осталось: <strong>${
+                hours < 0 ? (minutes == 0 ? hours + 24 : hours + 25) : hours
+            } часов ${minutes < 0 ? minutes + 60 : minutes} минут </strong>`,
             Markup.inlineKeyboard(
                 [Markup.callbackButton("➡️ Next", "next")],
                 {},
