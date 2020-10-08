f8cbe3797dcefa9aca9e8fc7470f40f1002d6f6a
bezgin.i <kurt1v@yandex.ru>
2020-10-08 17:31:53 +0300

    add cron job

--- src/service/controller/notification-controller.ts
+++ src/service/controller/notification-controller.ts
@@ -18,19 +18,24 @@ export const notificationController = ctx => {
     );
     job.start();
 
-    var job2 = new CronJob(
-        `${process.env.ALARM_HOUR} ${
-            Number(process.env.ALARM_MINUTE) + 5
-        } * * *`,
-        async function () {
-            await ctx?.telegram?.sendMessage(
-                Number(1111490838),
-                "Катенька обед не забыла?",
-            );
-        },
-        null,
-        true,
-        "Europe/Moscow",
-    );
-    job2.start();
+    // setInterval(async () => {
+    //     const currenctTime = moment().format("HH:mm");
+
+    //     if (currenctTime === featureTime) {
+    //         const data: any[] = await fetchDataController(ctx);
+
+    //         const chatIds = await data.map(elem => elem?.chat_id);
+
+    //         const userIds = await chatIds?.filter((elem, index, self) => {
+    //             return self.indexOf(elem) === index;
+    //         });
+
+    //         for (const id of userIds) {
+    //             await ctx.telegram.sendMessage(
+    //                 Number(id),
+    //                 "Катенька обед не забыла?",
+    //             );
+    //         }
+    //     }
+    // }, 540000);
 };
